529fd00f0cc4aa8d1977440b4a2266d99c813e31
saperoi <bizgames.shawn@gmail.com>
2022-01-25 18:37:48 +0100

    I GOT SHIT

--- main.py
+++ main.py
@@ -2,30 +2,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def f(x):
-  a = []
-  for i in x:
-    p = (-1)**math.ceil(i-d)
-    a.append(p)
-  return a
+# functions
 
-def run():
-  x = [-5, -4.75, -4.5, -4.25, -4, -3.75, -3.5, -3.25, -3, -2.75, -2.5, -2.25, -2, -1.75, -1.5, -1.25, -1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5]
+def fx_flop(x):
+  return (-1)**math.ceil(x-d)
+
+def fx_sin(x):
+  return (-1)**(x/math.pi - 0.5)
+  
+def fx_cos(x):
+  return (-1)**(x/math.pi)
+
+def fx_tan(x):
+  return ((-1)**(x/math.pi - 0.5))/(-1)**(x/math.pi - 0.5)
+  
+def fx_cot(x):
+  return (-1)**(x/math.pi - 0.5)/((-1)**(x/math.pi - 0.5))
+
+def fx_sec(x):
+  return 1/(-1)**(x/math.pi)
+
+def fx_csc(x):
+  return 1/(-1)**(x/math.pi - 0.5)
+
+def graph():
+  x = x_gen()
   global d
   d = 0
   while d <= 1.01:
     path = str(d) + '.png'
     plt.figure()
-    plt.plot(x,f(x), 'k')
-    plt.savefig(path)
+    plt.plot(x, [fx_csc(i) for i in x], 'k')
+    plt.savefig('csc/' + path)
     plt.close()
     print(str(d) + " completed")
     d += 0.04
+    d = round(d, 2)
 
-# reg = v * (-1)**math.ceil((x-d)/a) + s
+def x_gen():
+  j = []
+  i = round(-2.5*math.pi, 2)
+  while -2.5*math.pi <= i <= 2.5*math.pi:
+    j.append(i)
+    i += 0.01
+    i = round(i, 2)
+  return j
 
-# animi = x**((-1)**math.ceil(x-d))
-
-# animii = (-1)**math.ceil(x**((-1)**math.ceil(x-d)-d))
-
-run()
+graph()
