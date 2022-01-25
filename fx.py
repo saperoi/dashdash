0ac3be27b1ebbedf73637807d50c474ae826048a
saperoi <uesugi.sapero@gmail.com>
2022-01-25 08:19:46 +0100

    further plot wip

--- fx.py
+++ fx.py
@@ -2,6 +2,11 @@ import math
 
 reg = v * (-1)**ceil((x-d)/a) + s
 
-anim1 = x**((-1)**ceil(x-d))
+animi = x**((-1)**ceil(x-d))
 
-anim2 = (-1)**ceil(x**((-1)**ceil(x-d)-d)
+animii = (-1)**ceil(x**((-1)**ceil(x-d)-d)
+
+v = 1
+d = 0
+a = 1
+s = 0
--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import fx as floppy
 function GraphThat(f, name)
   m = mt.MathTextParser("Bitmap")
   m.to_png(name + '.png', f)
+
+fx.d = 0
+for fx.d <= 1:
+  GraphThat(fx.animone, fx.d)
+  fx.d += 0.04
